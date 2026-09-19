4f053784cb95e8486299720547f2faaaa21ae320
TingTing1990 <51083586+TingTing1990@users.noreply.github.com>
2026-09-19 22:49:36 +0800

    Fix import statement formatting in mieru.go
    Signed-off-by: TingTing1990 <51083586+TingTing1990@users.noreply.github.com>

--- option/mieru.go
+++ option/mieru.go
@@ -1,7 +1,6 @@
 package option
 
-import
-"github.com/sagernet/sing/common/json/badoption"
+import "github.com/sagernet/sing/common/json/badoption"
 
 type MieruOutboundOptions struct {
 	DialerOptions
