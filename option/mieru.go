2538b29de2fa0a934b02cb3759e74bd76fd4b5a4
TingTing1990 <51083586+TingTing1990@users.noreply.github.com>
2026-09-22 13:41:41 +0800

    Add files via upload
    Signed-off-by: TingTing1990 <51083586+TingTing1990@users.noreply.github.com>

--- option/mieru.go
+++ option/mieru.go
@@ -12,3 +12,16 @@ type MieruOutboundOptions struct {
 	Multiplexing     string                     `json:"multiplexing,omitempty"`
 	TrafficPattern   string                     `json:"traffic_pattern,omitempty"`
 }
+
+type MieruInboundOptions struct {
+	ListenOptions
+	Users               []MieruUser `json:"users,omitempty"`
+	Transport           string      `json:"transport,omitempty"`
+	TrafficPattern      string      `json:"traffic_pattern,omitempty"`
+	UserHintIsMandatory bool        `json:"user_hint_is_mandatory,omitempty"`
+}
+
+type MieruUser struct {
+	Name     string `json:"name,omitempty"`
+	Password string `json:"password,omitempty"`
+}
